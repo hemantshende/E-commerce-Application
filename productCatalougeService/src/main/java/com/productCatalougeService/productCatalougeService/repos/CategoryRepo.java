2d6def9ecd5af88d03220a30cbf08b65c008cf45
Hemant Shende <shendehc95@gmail.com>
2025-07-28 15:26:32 +0530

    added own storage service

---
+++ productCatalougeService/src/main/java/com/productCatalougeService/productCatalougeService/repos/CategoryRepo.java
@@ -0,0 +1,13 @@
+package com.productCatalougeService.productCatalougeService.repos;
+
+import org.example.productcatalogservice_june2025.models.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface CategoryRepo extends JpaRepository<Category,Long> {
+
+    Optional<Category> findById(Long id);
+}
